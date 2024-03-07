36882dc66427d1d27322bfe5322c411ee4df926d
Zvorky <enzzavorski@edu.univali.br>
2024-03-07 14:22:27 -0300

    women's day tribute to my beloved

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home(){
-    return <h1 align="center">Hello, World!</h1>
+    return (<div>
+                <h1>Feliz dia das Mulheres, Vida!</h1>
+                <div>Obrigado, Nátali, por fazer minha vida melhor, trazer-me conforto, incentivar, e ser feliz...</div>
+                <div>Você me inspira, sua dedicação é exemplar, não vejo a hora de poder dizer aos outros: </div>
+                <p>"Minha esposa tem artigos renomados publicados na área da Enfermagem e afins"...</p>
+                <h2>❤️ Te Amo! ❤️</h2>
+                <signature>~ Enzo</signature>
+            </div>)
 }
 
 export default Home;
