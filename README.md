dd8ed81df78b65bba7aabd261242bdb304d7914d
Zvorky <enzzavorski@edu.univali.br>
2024-03-09 17:14:36 -0300

    run `lint:fix`

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # <p align=center>TabNews Clone</p>
+
 Remake of [**TabNews**](https://www.tabnews.com.br/) with the aim of learning Web Development and gaining new experiences.
 
 - This project is part of the course [Curso.Dev](https://curso.dev/) by [Filipe Deschamps](https://github.com/filipedeschamps).
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-function Home(){
-    return (<div>
-                <h1>Feliz dia das Mulheres, Vida!</h1>
-                <div>Obrigado, Nátali, por fazer minha vida melhor, trazer-me conforto, incentivar, e ser feliz...</div>
-                <div>Você me inspira, sua dedicação é exemplar, não vejo a hora de poder dizer aos outros: </div>
-                <p>"Minha esposa tem artigos renomados publicados na área da Enfermagem e afins"...</p>
-                <h2>❤️ Te Amo! ❤️</h2>
-                <signature>~ Enzo</signature>
-            </div>)
+function Home() {
+  return (
+    <div>
+      <h1>Feliz dia das Mulheres, Vida!</h1>
+      <div>
+        Obrigado, Nátali, por fazer minha vida melhor, trazer-me conforto,
+        incentivar, e ser feliz...
+      </div>
+      <div>
+        Você me inspira, sua dedicação é exemplar, não vejo a hora de poder
+        dizer aos outros:{" "}
+      </div>
+      <p>
+        "Minha esposa tem artigos renomados publicados na área da Enfermagem e
+        afins"...
+      </p>
+      <h2>❤️ Te Amo! ❤️</h2>
+      <signature>~ Enzo</signature>
+    </div>
+  );
 }
 
 export default Home;
